2c19e0764feb37d6588dc415e16eec0b3d2717a9
hblake316 <26317600+hblake316@users.noreply.github.com>
2021-07-29 08:42:33 -0500

    test one value is greater than another

--- src/index.js
+++ src/index.js
@@ -1,63 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
 const validationSchema = yup.object({
-  email: yup
-    .string('Enter your email')
-    .email('Enter a valid email')
-    .required('Email is required'),
-  password: yup
-    .string('Enter your password')
-    .min(8, 'Password should be of minimum 8 characters length')
-    .required('Password is required'),
-});
+  value: yup.string('Enter Value').required('Required'),
+  endValue: yup
+    .string('Enter End Value')
+    .test(
+      'Compare Value and End Value',
+      'End Value has to be greater than Value.',
+      function (x) {
+        return x ? parseInt(x) > parseInt(this.parent.value) : true
+      },
+    ),
+})
 
 const WithMaterialUI = () => {
   const formik = useFormik({
     initialValues: {
-      email: 'foobar@example.com',
-      password: 'foobar',
+      value: '0101',
+      endValue: '2',
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+      alert(JSON.stringify(values, null, 2))
     },
-  });
+  })
 
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
         <TextField
           fullWidth
-          id="email"
-          name="email"
-          label="Email"
-          value={formik.values.email}
+          id="value"
+          name="value"
+          label="value"
+          value={formik.values.value}
           onChange={formik.handleChange}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
+          error={formik.touched.value && Boolean(formik.errors.value)}
+          helperText={formik.touched.value && formik.errors.value}
         />
         <TextField
           fullWidth
-          id="password"
-          name="password"
-          label="Password"
-          type="password"
-          value={formik.values.password}
+          id="endValue"
+          name="endValue"
+          label="endValue"
+          value={formik.values.endValue}
           onChange={formik.handleChange}
-          error={formik.touched.password && Boolean(formik.errors.password)}
-          helperText={formik.touched.password && formik.errors.password}
+          error={formik.touched.endValue && Boolean(formik.errors.endValue)}
+          helperText={formik.touched.endValue && formik.errors.endValue}
         />
-        <Button color="primary" variant="contained" fullWidth type="submit">
-          Submit
-        </Button>
+        <p>
+          <Button color="primary" variant="contained" fullWidth type="submit">
+            Submit
+          </Button>
+        </p>
       </form>
     </div>
-  );
-};
+  )
+}
 
-ReactDOM.render(<WithMaterialUI />, document.getElementById('root'));
+ReactDOM.render(<WithMaterialUI />, document.getElementById('root'))
